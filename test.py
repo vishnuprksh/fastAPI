1af8f645f40bd2f3224d61adb8b0d0883c8a37db
Vishnu Prakash <119521719+vishnuprksh@users.noreply.github.com>
2024-02-06 19:38:24 +0530

    bitfumes

---
+++ test.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Blog(BaseModel):
+    title: str
+    content : str
+    published : Optional[bool]
+
+
+
+@app.get("/get-blog")
+def get_blog(limit = 10, published: bool = True, sort : Optional[bool] = None):
+    return {"data": f"{limit} blogs from the list, where published equals {published}"}
+
+
+@app.post("/create-blog")
+def create_blog(blog: Blog):
+    return {"data": f"The blog is published with the title {blog.title}"}
